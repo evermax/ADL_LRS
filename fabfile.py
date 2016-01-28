305e9d402a8d7585e76cd102f515f48e250b2cef
Maxime <maxlasserre@free.fr>
2016-01-28 17:04:56 +0100

    remove fix

--- fabfile.py
+++ fabfile.py
@@ -8,22 +8,6 @@ def setup_env():
     for step in INSTALL_STEPS:
         local(step)
 
-    # Bug fix for django 1.4 package (not patched in d/l for some reason now)
-    try:
-        line_140 = linecache.getline('../env/local/lib/python2.7/site-packages/django/utils/translation/trans_real.py', 140)
-    except Exception:
-        line_140 = ""
-
-    if not line_140 or line_140 == '\n':
-        with open('../env/local/lib/python2.7/site-packages/django/utils/translation/trans_real.py', 'r') as f:
-            data = f.readlines()
-    
-        data[139] = "        if res is None:\n"
-        data[140] = "            return gettext_module.NullTranslations()\n"
-    
-        with open('../env/local/lib/python2.7/site-packages/django/utils/translation/trans_real.py', 'w') as f:
-            data = f.writelines(data)
-
 
 def setup_lrs():
     # Media folder names
@@ -31,13 +15,13 @@ def setup_lrs():
     activity_profile = 'activity_profile'
     activity_state = 'activity_state'
     statement_attachments = 'attachment_payloads'
-    
+
     # Add env packages and project to the path
     cwd = os.path.dirname(os.path.abspath(__file__))
-    
+
     if not cwd in sys.path:
         sys.path.append(cwd)
-    
+
     env_dir = os.path.join(cwd, '../env/lib/python2.7/site-packages')
     if not env_dir in sys.path:
         sys.path.append(env_dir)
